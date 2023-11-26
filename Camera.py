8d0b9a4794cb2df8fe61aa857428d894548627fb
Peer <peer200@gmx.de>
2023-11-26 14:25:37 +0100

    Change: Include the Ui Elements in the Class

--- Camera.py
+++ Camera.py
@@ -28,6 +28,47 @@ class CameraHandler:
         self.stopCamera()
         self.cam.close_device()
 
+    def initGUI(self,parentElement):
+        # adds the ui elements
+        with parentElement:
+            # The image / Camera Stuff
+            ui.label(text="CameraControls")
+            with ui.column():
+                ui.label("Camera Settings")
+                self.ui_settings_startstop = ui.switch("Start Acquisition",value=True,on_change=lambda c: self.startStopCamera())
+                self.ui_settings_externalTrigger = ui.switch("Use external Trigger")
+                self.ui_settings_time = ui.number(label="ExposureTime [ms]",value=2,on_change=lambda c: self.updateExposureTime())
+                self.ui_settings_framerate = ui.number(label="Framerate [1/s]",value=20,on_change=lambda c: self.updateFrameRate())
+            with ui.row():
+                self.ui_settings_save_propname = ui.input(label="Propellername")
+                self.ui_settings_save_rpm = ui.input(label="Trial RPM")
+                self.ui_settings_save_suffix = ui.input(label="Remark")
+                self.ui_settings_savebutton = ui.button(text="Save Image",on_click=lambda s: self.saveLatestImg())
+    def onNewImage(self,raw_image):
+        # handles the callback when a new image is available
+        # converts the image to opencv / numpy format
+        if raw_image.get_status() == gx.GxFrameStatusList.INCOMPLETE:
+            print("incomplete frame, wont process")
+        else:
+            cv_image = raw_image.get_numpy_array()
+            cv2.namedWindow("CameraFeed",cv2.WINDOW_NORMAL)
+            previewImage = cv2.resize(cv_image,(768,480))
+            cv2.imshow("CameraFeed",previewImage)
+            if(self.dosave):
+                # saves the Frame to Disk
+                self.dosave = False # clear Flag
+                datestamp = datetime.datetime.now().strftime("%y-%m-%d-%H:%M")
+                filename = self.Filename + "-" + datestamp + ".png"
+                cv2.imwrite(filename,cv_image) # saves the full resolution image
+
+    def startStopCamera(self):
+        startstop = self.ui_settings_startstop.value
+        if (startstop):
+            self.startCamera()
+        else:
+            self.stopCamera()
+
+
     def startCamera(self):
         self.cam.stream_on()
 
